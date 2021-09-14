b0af5c950e66dbab7657bce50216896d71708661
Starinin Andrey <starininandrey@gmail.com>
2021-09-14 15:55:32 +0300

    Init

---
+++ ObservableListLib/ObservableList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObservableListLib
+{
+    public class ObservableList
+    {
+        private List<int> _list;
+
+        public ObservableList()
+        {
+            _list = new List<int>();
+        }
+    }
+}
