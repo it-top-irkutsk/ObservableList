cadbe992b6f1c5c2182bde58b3f71ed65e297af5
Starinin Andrey <starininandrey@gmail.com>
2021-09-14 16:14:56 +0300

    Add regions

--- ObservableListLib/ObservableList.cs
+++ ObservableListLib/ObservableList.cs
@@ -5,11 +5,44 @@ namespace ObservableListLib
 {
     public class ObservableList
     {
+        #region Variable
+
         private List<int> _list;
+
+        #endregion
+
+        #region Constructor
 
         public ObservableList()
         {
             _list = new List<int>();
         }
+
+        public ObservableList(IEnumerable<int> list)
+        {
+            _list = new List<int>(list);
+        }
+
+        public ObservableList(ObservableList list)
+        {
+            _list = list.ToList();
+        }
+
+        #endregion
+
+
+        #region ExportMethod
+
+        public ObservableList CopyTo()
+        {
+            return new ObservableList(_list);
+        }
+
+        public List<int> ToList()
+        {
+            return _list;
+        }
+
+        #endregion
     }
 }
