253a6ebe7f97e640721595e4d7e00053312c08c7
Starinin Andrey <starininandrey@gmail.com>
2021-09-16 14:40:11 +0300

    Add testing project

---
+++ TestObservableListLib/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ObservableListLib;
+
+namespace TestObservableListLib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new ObservableList();
+            
+            list.Add(5);
+            list.Add(3);
+            list.Add(7);
+            
+            list.ForEach(Console.WriteLine);
+            list.ForEach(x=> x + 10);
+            list.ForEach(Console.WriteLine);
+        }
+    }
+}
