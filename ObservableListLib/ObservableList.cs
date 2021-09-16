c9333ad3f92f255333d5303cb4f7d6cde4eb2767
Starinin Andrey <starininandrey@gmail.com>
2021-09-16 14:39:48 +0300

    Add IClonable, ICollection<>, event

--- ObservableListLib/ObservableList.cs
+++ ObservableListLib/ObservableList.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ObservableListLib
 {
-    public class ObservableList
+    public class ObservableList : ICloneable, ICollection<int>
     {
         #region Variable
 
         private List<int> _list;
+
+        public event Action<ObservableList> Changed; 
+
+        public int Count { get; private set; }
+        public bool IsReadOnly { get; init; }
 
         #endregion
 
@@ -15,23 +22,38 @@ namespace ObservableListLib
         public ObservableList()
         {
             _list = new List<int>();
+            Count = 0;
+            IsReadOnly = false;
         }
 
         public ObservableList(IEnumerable<int> list)
         {
             _list = new List<int>(list);
+            Count = _list.Count;
+            IsReadOnly = false;
         }
 
         public ObservableList(ObservableList list)
         {
             _list = list.ToList();
+            Count = list.Count;
+            IsReadOnly = false;
         }
 
         #endregion
 
         #region ExportMethod
 
-        public ObservableList CopyTo()
+        public void CopyTo(int[] array, int arrayIndex)
+        {
+            //TODO Сделать проверки
+            for (int i = arrayIndex, j = 0; i < array.Length; i++, j++)
+            {
+                array[i] = _list[j];
+            }
+        }
+        
+        public object Clone()
         {
             return new ObservableList(_list);
         }
@@ -39,6 +61,66 @@ namespace ObservableListLib
         public List<int> ToList()
         {
             return _list;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Add(int item)
+        {
+            _list.Add(item);
+            Count++;
+            Changed?.Invoke(new ObservableList(_list));
+        }
+        
+        public bool Remove(int item) 
+        {
+            var res = _list.Remove(item);
+            Count--;
+            Changed?.Invoke(new ObservableList(_list));
+            return res;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+            Count = 0;
+            Changed?.Invoke(new ObservableList());
+        }
+
+        public bool Contains(int item) => _list.Contains(item);
+
+        #endregion
+        
+        public IEnumerator<int> GetEnumerator()
+        {
+            //TODO Реализовать GetEnumerator()
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //TODO Реализовать GetEnumerator()
+            return GetEnumerator();
+        }
+
+        #region ForEach
+
+        public void ForEach(Action<int> action)
+        {
+            foreach (var i in _list)
+            {
+                action(i);
+            }
+        }
+
+        public void ForEach(Func<int, int> func)
+        {
+            for (int i = 0; i < _list.Count; i++)
+            {
+                _list[i] = func(_list[i]);
+            }
         }
 
         #endregion
